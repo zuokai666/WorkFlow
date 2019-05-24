63e3fc9a292fde491d65a52bba67efffda363a5e
zuokai666 <13703391897@163.com>
2019-05-24 15:04:44 +0800

    解决springmvc显示不出html的问题，原来是约定大于配置的问题

--- workflow/src/main/java/org/zk/workflow/page/controller/UserController.java
+++ workflow/src/main/java/org/zk/workflow/page/controller/UserController.java
@@ -1,7 +1,6 @@
 package org.zk.workflow.page.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -9,7 +8,7 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/user")
 public class UserController {
 	
-	@RequestMapping(path="/login",method=RequestMethod.GET,produces="text/html")
+	@RequestMapping("/login")
 	public ModelAndView login(){
 		return new ModelAndView("login");
 	}
